ecfea3dae5f05562e9ddd897de39614f7f550c48
Grace Tueller <gracekamalei@gmail.com>
2022-04-21 15:38:36 -0400

    Update extract-aps-city-verification.py
    make excel list

--- monthly-deliverable-process/script/extract-aps-city-verification.py
+++ monthly-deliverable-process/script/extract-aps-city-verification.py
@@ -6,6 +6,7 @@
 import arcpy
 from datetime import datetime
 import shutil
+import pandas as pd
 
 arcpy.env.overwriteOutput = True
 
@@ -20,18 +21,27 @@ def exportFlagged(APs, city, domain_num, out_folder, RCLs, query_field):
     folderToZip = '{}\{}'.format(out_folder,name)
     arcpy.CreateFileGDB_management(folderToZip, name)
     
+    #select APs
     q = '{} = {}'.format(query_field, domain_num)
-
-    #select and export APs
     APselection = arcpy.SelectLayerByAttribute_management(APs, 'NEW_SELECTION', q)
 
     arcpy.FeatureClassToFeatureClass_conversion(APselection, r'{}\{}.gdb'.format(folderToZip, name), 'APsToVerify_{}_{}'.format(city, today))
 
     kmzPath = r'{0}\{1}.kmz'.format(folderToZip, 'APsToVerify_{}_{}'.format(city, today))
     arcpy.LayerToKML_conversion(APselection, kmzPath)
-
-    arcpy.AddMessage('gonna make an excel list :)')
-
+    #make excel list
+    excel_result = arcpy.TableToExcel_conversion(fc, out_folder + r'\APsToVerify_{}_{}.xls'.format(city, today))
+    excel_path = excel_result[0]
+    df = pd.read_excel(excel_path)
+    fieldstodrop = ['DiscrpAgID','DateUpdate','Effective','Expire','Country','State','County','AddDataURI','Inc_Muni','Uninc_Comm','Nbrhd_Comm','LSt_PreDir',
+                    'LSt_Name','LSt_Type','LSt_PosDir','ESN','MSAGComm','Post_Comm','Post_Code','Post_Code4','Building','Floor','Room','Seat','Addtl_Loc','LandmkName',
+                    'Mile_Post','Place_Type','Placement','Long','Lat','Elev','GC_Exception','created_user','created_date','last_edited_user','last_edited_date','GlobalID',
+                    'ADDRESS_ID','SEGMENT_ID','NAME_ID','SIDE','ANOMALY','UNIT_NUM','UNIT_TYPE','NOT MIGRATED']
+    df.drop(fieldstodrop, axis=1, inplace=True)
+    writer = pd.ExcelWriter(excel_path)
+    df.to_excel(writer, 'Sheet1')
+    arcpy.AddMessage("Saving Excel...")
+    writer.save()
 
     #select and export RCLs, if applicable
     if RCLs:
@@ -44,7 +54,7 @@ def exportFlagged(APs, city, domain_num, out_folder, RCLs, query_field):
     #zip folder 
     try:
         shutil.make_archive(folderToZip, 'zip', folderToZip)
-        arcpy.management.Delete(folderToZip)
+        #arcpy.management.Delete(folderToZip)
     except PermissionError as e:
         print(e)
         arcpy.AddMessage(e)
