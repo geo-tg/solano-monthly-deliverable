950147aab23677dce786bd8aab98c61edcddddf6
Grace Tueller <gracekamalei@gmail.com>
2022-04-26 08:16:27 -0400

    Update extract-aps-city-verification.py

--- monthly-deliverable-process/script/extract-aps-city-verification.py
+++ monthly-deliverable-process/script/extract-aps-city-verification.py
@@ -51,7 +51,7 @@ def exportFlagged(APs, city, domain_num, out_folder, RCLs, query_field):
             kmzPath = r'{0}\{1}.kmz'.format(folderToZip, 'APsToVerify_{}_{}'.format(city, today))
             arcpy.LayerToKML_conversion(APselection, kmzPath)
             #make list
-            excel_result1 = arcpy.TableToExcel_conversion(apfc, folderToZip + r'\APsToVerify_{}_{}.xlsx'.format(city, today))
+            excel_result1 = arcpy.TableToExcel_conversion(apfc, folderToZip + r'\APsToVerify_{}_{}.xlsx'.format(city, today), 'NAME', 'DESCRIPTION')
             excel_path1 = excel_result1[0]
             df1 = pd.read_excel(excel_path1)
             APfieldstodrop = ['OBJECTID', 'DiscrpAgID','DateUpdate','Effective','Expire','Country','State','County','AddDataURI','Inc_Muni','Uninc_Comm','Nbrhd_Comm','LSt_PreDir','LSt_Name','LSt_Type','LSt_PosDir','ESN','MSAGComm','Post_Comm','Post_Code','Post_Code4','Building','Floor','Room','Seat','Addtl_Loc','LandmkName','Mile_Post','Place_Type','Placement','Long','Lat','Elev','GC_Exception','created_user','created_date','last_edited_user','last_edited_date','GlobalID','ADDRESS_ID','SEGMENT_ID','NAME_ID','SIDE','ANOMALY','UNIT_NUM','UNIT_TYPE', 'AddCode']
@@ -67,7 +67,7 @@ def exportFlagged(APs, city, domain_num, out_folder, RCLs, query_field):
             kmzPath = r'{0}\{1}.kmz'.format(folderToZip, 'RCLsToVerify_{}_{}'.format(city, today))
             arcpy.LayerToKML_conversion(RCLselection, kmzPath)
             #make list
-            excel_result2 = arcpy.TableToExcel_conversion(rclfc, folderToZip + r'\RCLsToVerify_{}_{}.xlsx'.format(city, today))
+            excel_result2 = arcpy.TableToExcel_conversion(rclfc, folderToZip + r'\RCLsToVerify_{}_{}.xlsx'.format(city, today), 'NAME', 'DESCRIPTION')
             excel_path2 = excel_result2[0]
             df2 = pd.read_excel(excel_path2)
             RCLfieldstodrop = ['OBJECTID','LSt_PreDir','LSt_Name','LSt_Type','LSt_PosDir','ESN_L','ESN_R','MSAGComm_L','MSAGComm_R','Country_L','Country_R','State_L','State_R','County_L','County_R','IncMuni_L','IncMuni_R','UnincCom_L','UnincCom_R','NbrhdCom_L','NbrhdCom_R','PostCode_L','PostCode_R','PostComm_L','PostComm_R','OneWay','SpeedLimit','Valid_L','Valid_R','DiscrpAgID','DateUpdate','Effective','Expire','GC_Exception','created_user','created_date','last_edited_user','last_edited_date','GlobalID','SEGMENT_ID','NAME_ID','ANOMALY','fromElev','toElev','fromToCost','toFromCost', 'AddCode_L', 'AddCode_R', 'Shape_Length']
@@ -104,7 +104,7 @@ if __name__ == "__main__":
     RioVista = arcpy.GetParameter(5) 
     Fairfield = arcpy.GetParameter(6) 
     Vallejo = arcpy.GetParameter(7) 
-    Vacacille = arcpy.GetParameter(8)
+    Vacaville = arcpy.GetParameter(8)
     Suisun = arcpy.GetParameter(9)
     Dixon = arcpy.GetParameter(10)
     Unincorporated = arcpy.GetParameter(11) 
@@ -125,8 +125,8 @@ if __name__ == "__main__":
         if Vallejo:
             exportFlagged(APs, 'Vallejo', '6', out_folder, RCLs, query_field)
 
-        if Vacacille:
-            exportFlagged(APs, 'Vacacille', '7', out_folder, RCLs, query_field)
+        if Vacaville:
+            exportFlagged(APs, 'Vacaville', '7', out_folder, RCLs, query_field)
 
         if Suisun:
             exportFlagged(APs, 'Suisun', '8', out_folder, RCLs, query_field)
